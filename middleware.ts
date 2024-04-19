aea430d2a683d11c4bf08fbd6e22554cb75d7898
Luuk Santegoeds <luuk.santegoeds@avivasolutions.nl>
2024-04-19 13:08:33 +0200

    Added routes without middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/route-")) {
+  if (request.nextUrl.pathname.startsWith("/middleware")) {
     console.log("Hit middleware");
 
     request.nextUrl.searchParams.set("foo", "bar");
